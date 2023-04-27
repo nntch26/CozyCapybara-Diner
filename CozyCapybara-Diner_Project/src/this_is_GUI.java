3e099777989e516fef6a83e5bcd224ea4089473c
NTCH <117381190+nntch26@users.noreply.github.com>
2023-04-27 13:22:33 +0700

    Update this_is_GUI.java

--- CozyCapybara-Diner_Project/src/this_is_GUI.java
+++ CozyCapybara-Diner_Project/src/this_is_GUI.java
@@ -1,5 +1,13 @@
 public class this_is_GUI {
-    public static void main(String[] args) {
-        System.out.println("TEST TEST HELLO");
+
+    public this_is_GUI(){
+        System.out.println("ssssss");
     }
+    public static void main(String[] args) {
+        System.out.println("555555");
+    }
+
+   
+    
+    
 }
