91406fe53f85a25b148a993558949a8aefd6a405
Tumacsp <tumacsp@gmail.com>
2023-05-14 12:05:30 +0700

    Main and change some import

--- src/main/java/Table_PopUp.java
+++ src/main/java/Table_PopUp.java
@@ -1,8 +1,5 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.InternalFrameEvent;
 import javax.swing.event.InternalFrameListener;
