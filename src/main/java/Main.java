5353200e6d09bf3bee2e046991afa7ae013e8508
Tumacsp <tumacsp@gmail.com>
2023-05-20 23:11:57 +0700

    CheckBillGUI เหลือแค่ตกแต่ง จัดไฟล์กับแยก panel เรียบร้อย

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 import javax.swing.*;
 import static javax.swing.UIManager.*;
-//import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 
 
 public class Main {
     public static void main(String[] args) {
         try {
-            //setLookAndFeel(new FlatLightLaf() );
-            new LoginGUI();
+            setLookAndFeel(new FlatLightLaf());
+            new MainGUI();
 
         } catch( Exception ex ) {
             System.err.println( "Failed to initialize LaF" );
